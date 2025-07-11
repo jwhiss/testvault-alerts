b9bfd80b5753f39c348ec8f57d53f47cfbd7bb6f
jwhiss <JoelJWhissel@gmail.com>
2025-07-10 23:05:24 -0600

    remove os import

--- alertSender.py
+++ alertSender.py
@@ -14,7 +14,6 @@ PARTICULAR PURPOSE.  See the GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License along
 with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
-import os
 import smtplib
 import argparse
 import logging
